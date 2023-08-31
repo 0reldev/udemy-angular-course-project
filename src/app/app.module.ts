4ffc4488c3d63e7f6715922a9acc9619674ca9e7
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-08-31 15:56:15 +0200

    Creation of six new components
    Given the plan, creation of the six following components:
    - header
    - shopping-list
    - shopping-list-edit
    - recipe-list
    - recipe-item
    - recipe-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
