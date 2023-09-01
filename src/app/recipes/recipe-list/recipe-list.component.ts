a216da106c471bafc80aa78d899361dd48c2881f
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-09-01 15:10:51 +0200

    Addition of another recipe for test

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is simply a test', 'https://www.eatwell101.com/wp-content/uploads/2019/03/chicken-skillet-recipe-2-1460x2168.jpg')
+    new Recipe('A test recipe', 'This is simply a test', 'https://www.eatwell101.com/wp-content/uploads/2019/03/chicken-skillet-recipe-2-1460x2168.jpg'),
+    new Recipe('Second test recipe', 'This is another test', 'https://tse1.mm.bing.net/th?id=OIP.5LxHDVy2GcapI6esMjV53gHaJO&pid=Api')
   ];
 }
